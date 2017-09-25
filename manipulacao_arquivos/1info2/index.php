16e7d098e9751ff0158de1e9edd69dd141076e8b
rafaelsperoni <rafaelsperoni@gmail.com>
2017-09-25 14:33:45 -0300

    Cores nas linhas e contador
    Cores alternadas nas linhas
    Linha de cabeçalho
    Linha de Sumário com colspan

--- manipulacao_arquivos/1info2/index.php
+++ manipulacao_arquivos/1info2/index.php
@@ -8,18 +8,34 @@
 <?php
 	//funcao que abre um arquivo de texto e retorna o seu conteúdo como ARRAY (sendo cada posicao uma linha)
 	$agenda = file("agenda.csv");
-	
 
 	foreach($agenda as $chave=>$linha){
 		$colunas = explode(";", $linha);
-		print('<tr>
-			     <td>'.$colunas[0].'</td>
-			     <td>'.$colunas[1].'</td>
-			     <td>'.$colunas[2].'</td>
-			     <td>'.$colunas[3].'</td>
-			   </tr>');
+		if($chave == 0 ){//primeira linha
+            print('<tr>
+			         <th>'.$colunas[0].'</th>
+			         <th>'.$colunas[1].'</th>
+			         <th>'.$colunas[2].'</th>
+			         <th>'.$colunas[3].'</th>
+                   </tr>');
+		}else{
+			//teste para saber qual class vai escrever na TR
+			if(($chave % 2)==0){ //se for par
+				$cor = 'corPar';
+			}else{ //se for impar
+				$cor = 'corImpar';	
+			}		
+     		print('<tr class="'.$cor.'">
+			         <td>'.$colunas[0].'</td>
+			         <td>'.$colunas[1].'</td>
+			         <td>'.$colunas[2].'</td>
+			         <td>'.$colunas[3].'</td>
+			       </tr>');
+
+  		}
 	}
 ?>
+	  <tr><td colspan="4">X contato(s) listado(s)</td></tr>
     </table>
   </section>
 <?php
