27c60f544fd982bc0af097ca1231f7c533f364d6
Dicave <32268903+Dicave@users.noreply.github.com>
2017-09-26 15:19:05 -0300

    Add files via upload

---
+++ manipulacao_arquivos/1info3/1info3/ja.js
@@ -0,0 +1,12 @@
+function soma(){
+	
+var primeiro, segundo, terceiro;
+primeiro = parseInt(document.getElementById("primeiro").value);
+
+segundo = parseInt(document.getElementById("segundo").value);
+
+total = primeiro + segundo;
+
+document.getElementById("resultado").innerHTML = total;
+
+}
